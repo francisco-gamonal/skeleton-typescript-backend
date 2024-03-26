dd1f3dfc57e7b8d6a0db9333dd4d71f0a511c337
Francisco Gamonal <hfgamonalb@gmail.com>
2024-03-25 23:15:25 -0500

    feat: add alias-tsc on app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,8 @@
-import sum from './sum';
+import sum from '@/sum';
+import average from '@/average';
 
 const result = sum(1, 2);
 console.log(result);
+
+const result2 = average(1, 2);
+console.log(result2);
